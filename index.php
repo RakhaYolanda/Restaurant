930aa07e9eadf7b320f00797776609c1bbe72264
unknown <rakhak410@gmail.com>
2020-02-18 09:44:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Warteg</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<br/>
+	<br/>
+	<center><h2>WELCOME TO IJUP WARTEG</h2></center>	
+	<br/>
+	<div class="login">
+	<br/>
+		<form action="menu.php" method="post">
+			<div>
+				<label>Username:</label>
+				<input type="text" name="username" id="username" />
+			</div>
+			<div>
+				<label>Password:</label>
+				<input type="password" name="password" id="password" />
+			</div>			
+			<div>
+				<input type="submit" value="Login" class="tombol">
+			</div>
+		</form>
+	</div>
+
+</body>
+</html>
---
+++ terimakasih.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.gambar {
+  background-image: url("background6.jpg");
+  height: 740px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.tulisan {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+</style>
+</head>
+
+<body>
+
+<div class="gambar">
+  <div class="tulisan">
+    <h1 style="font-size:70px">-THANK YOU-</h1>
+    <h3 style="height:300px">TELAH MEMESAN DI WARTEG IJUP</h3>
+  </div>
+</div>
+</body>
+</html>
